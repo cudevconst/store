4a8a15bff5c7d81106245633e87587b84a95f91a
Nguyễn Văn Cường <cuong18092001@gmail.com>
2023-08-28 22:41:48 +0700

    Update CloudinaryConfig.java

--- src/main/java/com/example/nvcshop/config/CloudinaryConfig.java
+++ src/main/java/com/example/nvcshop/config/CloudinaryConfig.java
@@ -9,15 +9,15 @@ import java.util.Map;
 
 @Configuration
 public class CloudinaryConfig {
-//    private final String CLOUD_NAME = "YOUR_CLOUD_NAME";
-//    private final String API_KEY = "YOUR_API_KEY";
-//    private final String API_SECRET = "YOUR_SECRET_KEY";
+    private final String CLOUD_NAME = "YOUR_CLOUD_NAME";
+    private final String API_KEY = "YOUR_API_KEY";
+    private final String API_SECRET = "YOUR_SECRET_KEY";
     @Bean
     public Cloudinary cloudinary(){
         Map<String, String> config = new HashMap<>();
-        config.put("cloud_name","dfbohi05o");
-        config.put("api_key","548959173139792");
-        config.put("api_secret","USoecGtt-YMqVJbjlOsTkQ7gQiM");
+        config.put("cloud_name",CLOUD_NAME);
+        config.put("api_key",API_KEY);
+        config.put("api_secret",API_SECRET);
 
         return new Cloudinary(config);
     }
